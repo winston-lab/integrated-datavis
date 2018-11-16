13c458b66cfe9f29989ec7d5086ff72ce276836c
James Chuang <chuang.ja@gmail.com>
2018-11-16 16:33:32 -0500

    Update slurm commands and cluster requests.

--- slurm.sh
+++ slurm.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-#SBATCH -p medium
-#SBATCH -t 24:00:00
-#SBATCH --mem-per-cpu=4G
+#SBATCH -p short
+#SBATCH -t 12:00:00
+#SBATCH --mem-per-cpu=1G
 #SBATCH -n 1
 #SBATCH -e snakemake.err
 #SBATCH -o snakemake.log
 #SBATCH -J data-integration-snakemake
 
-snakemake -p -R `cat <(snakemake --lc) <(snakemake --li) <(snakemake --lp)` --latency-wait 300 --rerun-incomplete --cluster-config cluster.yaml --use-conda --jobs 999 --restart-times 1 --cluster "sbatch -p {cluster.queue} -n {cluster.n} -t {cluster.time} --mem-per-cpu={cluster.mem} -J {cluster.name} -e {cluster.err} -o {cluster.log}"
+snakemake -p -R `cat <(snakemake --lc --rerun-incomplete) <(snakemake --li --rerun-incomplete) <(snakemake --lp --rerun-incomplete) | sort -u` --latency-wait 300 --rerun-incomplete --cluster-config cluster.yaml --use-conda --jobs 999 --restart-times 1 --cluster "sbatch -p {cluster.queue} -n {cluster.n} -t {cluster.time} --mem-per-cpu={cluster.mem} -J {cluster.name} -e {cluster.err} -o {cluster.log}"
